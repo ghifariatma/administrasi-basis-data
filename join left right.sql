1b61f3a8d23ea5d8788d41d5052add5aac9d0c80
ghifariatma <121796041+ghifariatma@users.noreply.github.com>
2023-07-01 12:07:28 +0800

    Add files via upload

---
+++ join left right.sql
@@ -0,0 +1,29 @@
+
+use db_pratikum;
+
+-- join
+desc ms_pelanggan_dqlab;
+desc tr_penjualan_dqlab;
+
+-- mengambil kolom kode pelanggan, nama pelanggan, nama produk,. qty berdasarkan kode pelanggan
+select tp.kode_pelanggan, mp.nama_pelanggan, tp.nama_produk, tp.qty
+from tr_penjualan_dqlab as tp
+join ms_pelanggan_dqlab as mp
+on tp.kode_pelanggan = mp.kode_pelanggan;  
+
+-- cross join
+-- ambil kolom nama_pelangan, nama_produk, qty tanpa relationship keys
+select mp.nama_pelanggan, tp.nama_produk, tp.qty
+from tr_penjualan_dqlab as tp
+join ms_pelanggan_dqlab as mp
+on true;
+
+-- left outer join
+-- ambil nama_pelanggan, nama_produk, dan qty dengan left outer join
+select tp.kode_pelanggan, mp.nama_pelanggan, tp.qty from tr_penjualan_dqlab as tp left outer join ms_pelanggan_dqlab as mp
+on tp.kode_pelanggan = mp.kode_pelanggan;
+
+-- right outer join
+-- ambil nama_pelanggan, nama_produk, dan qty dengan right outer join
+select tp.kode_pelanggan, mp.nama_pelanggan, tp.qty from tr_penjualan_dqlab as tp right outer join ms_pelanggan_dqlab as mp
+on tp.kode_pelanggan = mp.kode_pelanggan;
